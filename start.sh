b24eee03f1bae2d05a1c71f55cc6d0ce638d1b29
Luxxy-GF <deniedaddicted@gmail.com>
2022-05-18 12:33:48 +0000

    what

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 echo running start.sh
+
+# if [ -n "${START_COMMAND1}" ]; then 
+#     eval "${START_COMMAND1}"
+# fi
 eval ${START_COMMAND1}
 if [ -n "${START_COMMAND2}" ]; then
     eval ${START_COMMAND2}
