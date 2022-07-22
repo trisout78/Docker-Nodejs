80e1b63f48108f46f1fd5e65c7c94681e4e8e3f5
Luxxy-GF <68338577+Luxxy-GF@users.noreply.github.com>
2022-07-22 22:38:55 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,49 +20,4 @@ echo -e "${CYAN}STARTUP /home/container: /start.sh ${RESET_COLOR}"
 
 clear
 
-echo -e "${CYAN} 
- _                                 _     _                 _             
-| |                               | |   | |           _   (_)            
-| |     _   _ _   _ _   _ _   _   | |__ | | ___   ___| |_  _ ____   ____ 
-| |    | | | ( \ / | \ / ) | | |  |  __)| |/ _ \ /___)  _)| |  _ \ / _  |
-| |____| |_| |) X ( ) X (| |_| |  | |   | | |_| |___ | |__| | | | ( ( | |
-|_______)____(_/ \_|_/ \_)\__  |  |_|   |_|\___/(___/ \___)_|_| |_|\_|| |
-                         (____/                                   (_____|
-${RESET_COLOR}"
-
-sleep 1.5
-
-echo -e "\e[92m* \e[39m[\e[94mINFO\e[39m] Server \e[4m\e[95mStats\e[39m\e[0m"
-
-echo -e ""
-
-sleep 1.5
-
-echo -e "\e[92m* \e[39m[\e[92mSTATS\e[39m] RAM ->  \e[4m\e[95m"${SERVER_MEMORY}
-
-sleep 1
-
-echo -e "\e[92m* \e[39m[\e[92mSTATS\e[39m] LOCATION ->  \e[4m\e[95m"${P_SERVER_LOCATION}
-
-sleep 1
-
-echo -e "\e[92m* \e[39m[\e[92mSTATS\e[39m] SERVER ID ->  \e[4m\e[95m"${P_SERVER_UUID}
-
-sleep 1
-
-echo -e "\e[92m* \e[39m[\e[92mSTATS\e[39m] SERVER IP ->  \e[4m\e[95m"${SERVER_IP}
-
-sleep 1
-
-echo -e "\e[92m* \e[39m[\e[92mSTATS\e[39m] SERVER PORT ->  \e[4m\e[95m"${SERVER_PORT}
-
-sleep 1
-
-echo -e "\e[92m* \e[39m[\e[92mSTATS\e[39m] NODEJS VERISON ->  \e[4m\e[95m"${NODE_VERSION}
-
-sleep 1.5
-
-echo -e "\e[92m* \e[39m[\e[94mINFO\e[39m] Starting Nodejs installation..."
-
-
 eval '/start.sh'
