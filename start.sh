df15eeb66af3d3da06f55548af7cc5f807d3ee30
Trisout78 <trisout78@gmail.com>
2025-08-20 22:28:53 +0200

    Refactor start.sh to include a banner and improve command evaluation syntax

--- start.sh
+++ start.sh
@@ -1,8 +1,18 @@
-echo running start.sh
+#!/usr/bin/env bash
+
+# Banner ASCII TRISOUT
+cat <<'BANNER'
+ _____     _                 _   
+|_   _| __(_)___  ___  _   _| |_ 
+  | || '__| / __|/ _ \| | | | __|
+  | || |  | \__ \ (_) | |_| | |_ 
+  |_||_|  |_|___/\___/ \__,_|\__|
+                                 
+BANNER
 
 if [ -n "${START_COMMAND1}" ]; then 
     eval "${START_COMMAND1}"
 fi
 if [ -n "${START_COMMAND2}" ]; then
-    eval ${START_COMMAND2}
+    eval "${START_COMMAND2}"
 fi
